07fef01e8c4003c867d510b43fdcefb1d48b23c6
Romain Milon <rmilon@gmail.com>
2020-02-20 19:39:38 +0100

    Finished evalutation function

--- test.py
+++ test.py
@@ -37,18 +37,32 @@ def model(config, solution):
     libraries_order = solution["libraries_order"] # the order in which librairies will sign up
     if len(libraries_order) > 0:
         libraries_signed = []
-        library_index = 0 # index of the library signing up
+        library_index = 0 # index (in libraries_order) of the library currently signing up
+        library_signup_delay = config["libraries"][library_index]["signup_time"]
         for t in range(config["days"]):
+            if library_index is not None:
+                library_signup_delay -= 1
+
+                if library_signup_delay <= 0:
+                    libraries_signed.append(libraries_order[library_index])
+                    if len(libraries_order) > library_index + 1:
+                        library_index += 1
+                        library_signup_delay = config["libraries"][library_index]["signup_time"]
+                    else:
+                        library_index = None
+
             for library_id in libraries_signed:
                 velocity = config["libraries"][library_id]["velocity"]
-                shipped_books = solution["libraries"]
+                shipped_books = solution["libraries"][:velocity]
+                score += sum(map(lambda book_id: config["books"][book_id], shipped_books))
     
     return score
 
-def is_valid(solution):
+def is_valid(config, solution):
     for library_id in solution["libraries"]:
         assert set(solution["libraries"][library_id]["books_order"]).issubset(config["books"]), "Library {} ships a book that doesn't exist!".format(library_id)
         assert set(solution["libraries"][library_id]["books_order"]).issubset(config["libraries"][library_id]["books"]), "Library {} ships a book that it doesn't own!".format(library_id)
 
 def compute(config, solution):
     is_valid(config, solution)
+    return model(config, solution)
